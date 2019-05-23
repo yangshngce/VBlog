fa92eb6cb9e0b71076e97702d6d555ea191250d5
yuyy <326018984@qq.com>
2019-05-23 16:58:28 +0800

    my test

--- src/main/java/controller/te.java
+++ src/main/java/controller/te.java
@@ -2,12 +2,12 @@ package controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping("/home")
+@RestController
 public class te {
-
-    @RequestMapping("/index")
+//sadasdasd
+    @RequestMapping("index")
     public String index() {
         return "index";
     }
