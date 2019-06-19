b81a6ca1f858963404b321b84b709aa4cdcec24d
yuyy <326018984@qq.com>
2019-06-20 02:23:39 +0800

    初始化首页，显示视频名，上传者，上传时间

--- src/main/webapp/WEB-INF/statics/js/comment.js
+++ src/main/webapp/WEB-INF/statics/js/comment.js
@@ -1,19 +1,39 @@
 $(function(){
-
+    var type="get";
+    var url="http://localhost:8080/VBlog_war/commentTest";
+    console.log(data);
+    $("#submit").myAjaxGet(url,data,function(da){
+        console.log(da);
+        for (var i=0;i<da.length;i++){
+            $("#list").append("<li>"+da[i].usrName+da[i].createTime+da[i].content+"</li>")
+        }
+    },type);
     $("#submit").click(function(){
+
+        var url="http://localhost:8080/VBlog_war/session";
+        var data=null;
+        $.ajax({
+            url:url,
+            data:data?data:{},
+            datatype:"json",
+            type:"get",
+            success:function(da){
+                console.log(da);
+                if (da.code == "3001") { //判断返回值，这里根据的业务内容可做调整
+                    $("#personalA").text("PersonalZone");
+                    $("#personalA").attr('href',"statics/yuyy_html_test/personalZone.html");
+                } else if (da.code == "3000"){
+                    alert("请先登陆");
+                    window.location.href =
+                        "http://localhost:8080/VBlog_war";//登录后的页面地址
+                }
+            }
+        });
+
         var yourcomment = $("#your-comment").prop("value");
         var data={ videoId: "10002" };
         console.log(data);
         alert("000");
-        var type="get";
-        var url="http://localhost:8080/VBlog_war/commentTest";
-        console.log(data);
-        $("#submit").myAjaxGet(url,data,function(da){
-            console.log(da);
-            for (var i=0;i<da.length;i++){
-                $("#list").append("<li>"+da[i].usrName+da[i].createTime+da[i].content+"</li>")
-            }
-        },type);
     });
 })
 
